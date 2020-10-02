a63a2427f22e094d9a2100ac05f714f64e42bd5a
guihRovetta <guilherme020213@hotmail.com>
2020-10-01 22:36:29 -0300

    🚧 create game service populate

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (params) => {
+    const cat = await strapi.services.category.find({ name: "Action" });
+
+    console.log(cat);
+  },
+};
